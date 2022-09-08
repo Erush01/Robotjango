2a540a7919e34283c9cec4c6c4300e0721125997
Erush01 <69166608+Erush01@users.noreply.github.com>
2022-09-08 18:50:45 +0300

    Add files via upload

--- robot_django/settings.py
+++ robot_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = "django-insecure-)j2n7m!yh*bmk6kdaed8de_e!$sgk58_^!se=_zihs0rl(1rag"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
